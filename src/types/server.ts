6a9b4c795c886c6a1f666f128ae666445da847da
Harminder Virk <virk.officials@gmail.com>
2023-02-14 11:20:36 +0530

    style: remove unused types

--- src/types/server.ts
+++ src/types/server.ts
@@ -7,7 +7,6 @@
  * file that was distributed with this source code.
  */
 
-import type { ContainerResolver } from '@adonisjs/fold'
 import type { ErrorHandler, FinalHandler } from '@poppinss/middleware/types'
 
 import type { Constructor } from './base.js'
