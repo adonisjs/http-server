ac44a0ff5dbc223f3610b88ffeaff46191ac99a5
Romain Lanz <romain.lanz@pm.me>
2026-07-27 09:02:28 +0200

    style: run formatting

--- src/client/url_builder.ts
+++ src/client/url_builder.ts
@@ -21,8 +21,7 @@ export { createURL, findRoute }
  */
 export function createUrlBuilder<Routes extends LookupList>(
   routesLoader:
-    | { [domain: string]: ClientRouteJSON[] }
-    | (() => { [domain: string]: ClientRouteJSON[] }),
+    { [domain: string]: ClientRouteJSON[] } | (() => { [domain: string]: ClientRouteJSON[] }),
   searchParamsStringifier: (qs: Record<string, any>) => string,
   defaultOptions?: URLOptions
 ): UrlFor<Routes> {
