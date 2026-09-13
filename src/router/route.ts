0e13d74c268b776263a0b039008bdda44902e271
Romain Lanz <romain.lanz@pm.me>
2026-09-13 14:28:16 +0000

    style: format with latest prettier

--- src/router/route.ts
+++ src/router/route.ts
@@ -144,9 +144,7 @@ export class Route<Controller extends Constructor<any> = any> extends Macroable 
       pattern: string
       methods: string[]
       handler:
-        | RouteFn
-        | string
-        | [LazyImport<Controller> | Controller, GetControllerHandlers<Controller>?]
+        RouteFn | string | [LazyImport<Controller> | Controller, GetControllerHandlers<Controller>?]
       globalMatchers: RouteMatchers
     }
   ) {
@@ -174,9 +172,7 @@ export class Route<Controller extends Constructor<any> = any> extends Macroable 
    */
   #resolveRouteHandle(
     handler:
-      | RouteFn
-      | string
-      | [LazyImport<Controller> | Controller, GetControllerHandlers<Controller>?]
+      RouteFn | string | [LazyImport<Controller> | Controller, GetControllerHandlers<Controller>?]
   ) {
     /**
      * Convert magic string to handle method call
